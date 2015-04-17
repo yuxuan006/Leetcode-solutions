77dd2630ca033165ce41aea986f871dad905138c
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-04-17 14:19:34 -0400

    Climbing Stairs

---
+++ climbStairs.cpp
@@ -0,0 +1,24 @@
+//Leetcode c++
+//Climbing Stairs
+/*
+ You are climbing a stair case. It takes n steps to reach to the top.
+ 
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 0)return 0;
+        if(n == 1)return 1;
+        if(n == 2)return 2;
+        int res;
+        int a = 1, b = 2;
+        for(int i = 3; i <= n; i++){
+            res = a + b;
+            a = b;
+            b = res;
+        }
+        return res;
+    }
+    
+};
