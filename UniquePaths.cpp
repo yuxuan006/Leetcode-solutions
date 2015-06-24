9909c5b293071b6e447ff87a4d9a0410aa784c8c
yuchai006 <xuan@Yuxuan-Chais-MacBook-Air.local>
2015-06-23 21:22:01 -0400

    Unique Paths

---
+++ UniquePaths.cpp
@@ -0,0 +1,23 @@
+//Leetcode c++
+//Unique Paths 
+
+/**
+ A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ 
+ The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ 
+ How many possible unique paths are there?
+ */
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> res(m,vector<int>(n,1));
+        for(int i = 1; i < m; ++i){
+            for(int j = 1; j < n; ++j){
+                res[i][j] = res[i-1][j] + res[i][j-1];
+            }
+        }
+        return res[m-1][n-1];
+    }
+    
+};
